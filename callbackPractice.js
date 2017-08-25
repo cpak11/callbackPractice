fe6474c26bfb422b1c5ce27247cecdef0f4d766a
cpak11 <c.pak81@gmail.com>
2017-08-25 17:31:15 -0400

    brain... mal...func...ti...i...on...

--- callbackPractice.js
+++ callbackPractice.js
@@ -1,14 +1,14 @@
 /* In this repo your job is to write functions to make each function call work properly.
-Below is a sample problem 
+Below is a sample problem
 
   //code here for sayHi
 
    sayHi('Hi Katie', function(thingToSay){
       alert(thingToSay);
    });
-   
-and what you should write is the sayHi function that makes the code above work, 
-    
+
+and what you should write is the sayHi function that makes the code above work,
+
    var sayHi = function(str, cb){
     cb(str);
    }
@@ -16,27 +16,31 @@ and what you should write is the sayHi function that makes the code above work,
    sayHi('Hi Katie', function(thingToSay){
       alert(thingToSay); //should alert ('Hi Katie')'
    });
-    
+
 */
 
 
 // 1. Write a function called first that returns the first item of the array using a callback function
 
   // Code Here
+function first(arr, cb){
+  cb(arr[0]);
+}
 
-  
 var names = ['Tyler', 'Cahlan', 'Ryan', 'Colt', 'Tyler', 'Blaine', 'Cahlan'];
+
 first(names, function(firstName){
   console.log('The first name in names is ' + firstName);
   return firstName;
 });
 
 
-
 // 2. Write a function called last which returns the last item of the array using a callback function.
 
   //Code Here
-
+function last(arr,cb){
+  cb(arr[arr.length-1]);
+}
 
 
 last(names, function(lastName){
@@ -46,10 +50,13 @@ last(names, function(lastName){
 
 
 
-// 3. Write a function called multiply that multiplies two numbers. Invoke the callback with the result of the multiplication. 
+// 3. Write a function called multiply that multiplies two numbers. Invoke the callback with the result of the multiplication.
 
   //Code Here
-
+function multiply(a,b,cb){
+  var ans = a*b;
+  cb(ans);
+}
 
 
 multiply(4, 3, function(answer){
@@ -58,12 +65,17 @@ multiply(4, 3, function(answer){
 
 
 
-// 4. Write a function called contains that checks if a name exists in an array. 
-// If it does, invoke the callback with true as an argument. 
+// 4. Write a function called contains that checks if a name exists in an array.
+// If it does, invoke the callback with true as an argument.
 // If the name does not exist, invoke the callback with false as an argument.
 
-  //Code Here 
-
+  //Code Here
+function contains(arr, name, cb){
+  for(i=0;i<arr.length;i++){
+    if(name === arr[i]){cb(true);}
+  }
+  return cb(false);
+}
 
 
 
@@ -81,8 +93,16 @@ contains(names, 'Colt', function(result){
 // Invoke the callback with the modified array as an argument.
 
   //Code Here
-
-
+function uniq(arr,cb){
+  arr = arr.sort();
+  for(var i=0;i<arr.length;i++){
+    if(arr[i]===arr[i + 1]){
+      arr.splice(i,1);
+      i--
+    }
+  }
+  return cb(arr);
+}
 
 uniq(names, function(uniqArr){
   console.log('The new names array with all the duplicate items removed is ', uniqArr);
@@ -91,8 +111,11 @@ uniq(names, function(uniqArr){
 
 // 6. Write a function called each that takes in an array of names. For each name in the array, invoke the callback and pass in the name and the name's index as arguments.
 
-    //Code Here 
+    //Code Here
+function each(arr,cb){
+  for(i=0;i<arr.length;i++){cb(arr[i],arr[i].indexOf());}
 
+}
 
 
 each(names, function(item, indice){
@@ -105,7 +128,11 @@ each(names, function(item, indice){
 // When the correct user object is found, invoke the callback with the user object as an argument.
 
 // Code here
-
+function getUserById(arr,id,cb){
+  for(i=0;i<arr.length;i++){
+    if(id===arr[i].id){cb(arr[i]);}
+  }
+}
 
 
 var users = [
@@ -130,5 +157,5 @@ var users = [
 ];
 
 getUserById(users, '16t', function(user){
-  console.log('The user with the id 16t has the email of ' + user.email + ' the name of ' + user.name + ' and the address of ' + user.address); 
+  console.log('The user with the id 16t has the email of ' + user.email + ' the name of ' + user.name + ' and the address of ' + user.address);
 });
